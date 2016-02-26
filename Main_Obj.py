3f24b92c5e771476ff36f3cc41842c92b4d47c64
kraudust <dustan.kraus@gmail.com>
2016-02-26 16:17:40 -0700

    commented the obj function and changed some variable names

--- Main_Obj.py
+++ Main_Obj.py
@@ -8,20 +8,32 @@ from VAWT_loss_calculation import *
 
 def obj(xin,params):
     """
-
-    :param xin:
-    :param params:
+	inputs:
+		xin - nparray with x, and y locations for each turbine. Form is [xVAWT, yVAWT, xHAWT, yHAWT]
+		params - parameters that are kept constant
+			nVAWT - number of vertical axis turbines
+			rh - radius of horizontal axis turbines
+			rv - radius of vertical axis turbines
+			rt - radius of tower
+			U_dir - wind direction in radians
+			U_vel - free stream velocity magnitude
     :return:
+    	Ptotal: total power output
     """
-    nTurbs = len(xin)/2
-    nVAWT, r_h, r_v, r_t, U_direction_radians, U_velocity = params
-    nHAWT = nTurbs - nVAWT
+    nTurbs = len(xin)/2 #total number of turbines
+    nVAWT, rh, rv, rt, U_dir, U_vel = params
+    nHAWT = nTurbs - nVAWT #number of horizontal axis turbines
+    
+    #split xin into x and y locations for VAWT and HAWT
     xVAWT = xin[0 : nVAWT]
     xHAWT = xin[nVAWT : nTurbs]
     yVAWT = xin[nTurbs : nTurbs + nVAWT]
     yHAWT = xin[nTurbs + nVAWT : len(xin)]
-
+	
+	#calculate power for VAWT and HAWT
     HAWT_Power = Jensen_Wake_Model(xHAWT, yHAWT, params)
     VAWT_Power = VAWT_Power_Func(xVAWT, yVAWT, params)
-
-    return HAWT_Power+VAWT_Power
+	
+	Ptotal = HAWT_Power + VAWT_Power
+    return Ptotal
+    
