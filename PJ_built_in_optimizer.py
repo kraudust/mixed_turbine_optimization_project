2de347a561f4379ac323f90e65495ab91e00bc59
PJ Stanley <stanley_andrewpj@yahoo.com>
2016-02-27 22:09:12 -0700

    PJ's optimization

--- PJ_built_in_optimizer.py
+++ PJ_built_in_optimizer.py
@@ -5,56 +5,47 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from Main_Obj import *
 
-def con(xin):
-    x = np.zeros(nTurbines)
-    y = np.zeros(nTurbines)
-    x[:] = xin[0:nTurbines]
-    y[:] = xin[nTurbines:nTurbines*2]
-    constraints = np.array([])
-    for i in range(len(x)):
-        for j in range(len(x)):
-            constraints = np.append(constraints, sqrt(x[i]**2+y[j]**2)-10*r_0)
-
-    return constraints
-
 if __name__=="__main__":
-    "Define Variables"
-    xHAWT = np.array([0, 0, 0, 200, 200, 200, 400, 400, 400])
-    yHAWT = np.array([0, 200, 400, 0, 200, 400, 0, 200, 400])
-    xVAWT = np.array([100, 100, 100, 300, 300, 300])
-    yVAWT = np.array([100, 200, 300, 100, 200, 300])
+	"Define Variables"
+	xHAWT = np.array([0, 0, 0, 200, 200, 200, 400, 400, 400])
+	yHAWT = np.array([0, 200, 400, 0, 200, 400, 0, 200, 400])
+	xVAWT = np.array([100, 100, 100, 300, 300, 300])
+	yVAWT = np.array([100, 200, 300, 100, 200, 300])
 
-    xin = np.hstack([xVAWT, yVAWT, xHAWT, yHAWT])
-    nVAWT = len(xVAWT)
-    rh = 40.
-    rv = 3.
-    rt = 5.
-    direction = 30.
-    dir_rad = (direction+90) * np.pi / 180.
-    U_vel = 8.
+	xin = np.hstack([xVAWT, yVAWT, xHAWT, yHAWT])
+	nVAWT = len(xVAWT)
+	rh = 40.
+	rv = 3.
+	rt = 5.
+	direction = 30.
+	dir_rad = (direction+90) * np.pi / 180.
+	U_vel = 8.
 
-    params = [nVAWT, rh, rv, rt, dir_rad, U_vel]
+	params = nVAWT, rh, rv, rt, dir_rad, U_vel
+	
 
-    bounds = np.empty((nTurbines*2.,2))
-    bounds[0:nTurbines, :] = np.array([0, 1000])
-    bounds[nTurbines:nTurbines*2, :] = np.array([0, 1000])
-    options = {'disp': True}
-    constraints = {'type': 'ineq', 'fun': con}
-    res = minimize(obj, xin, method='SLSQP', jac=False, bounds=bounds, tol=1e-6, constraints=constraints, options=options)
+	print obj(xin, params)
+	print con(xin, params)
 
-    print res.x
-    print con(res.x)
+	boundaries = bounds(xin, params, 1500, 1500)
+	print boundaries
+	options = {'disp': True}
+	constraints = {'type': 'ineq', 'fun': con, 'args': (params)}
+	res = minimize(obj, xin, args=[params], method='SLSQP', jac=False, bounds=boundaries, tol=1e-6, constraints=constraints, options=options)
 
-    xcoordinates = np.zeros(nTurbines)
-    ycoordinates = np.zeros(nTurbines)
+	print res.x
+	print con(res.x)
 
-    xcoordinates = res.x[0:nTurbines]
-    ycoordinates = res.x[nTurbines:nTurbines*2.]
+	xcoordinates = np.zeros(nTurbines)
+	ycoordinates = np.zeros(nTurbines)
 
-    plt.scatter(x,y,label='Start')
-    plt.scatter(xcoordinates,ycoordinates, c='r', label='Optimized')
-    plt.legend()
-    plt.show()
+	xcoordinates = res.x[0:nTurbines]
+	ycoordinates = res.x[nTurbines:nTurbines*2.]
 
-    print "Start Power: ", Jensen_Wake_Model(xin, params)
-    print "Optimized Power: ", Jensen_Wake_Model(res.x, params)
+	plt.scatter(x,y,label='Start')
+	plt.scatter(xcoordinates,ycoordinates, c='r', label='Optimized')
+	plt.legend()
+	plt.show()
+
+	print "Start Power: ", Jensen_Wake_Model(xin, params)
+	print "Optimized Power: ", Jensen_Wake_Model(res.x, params)
