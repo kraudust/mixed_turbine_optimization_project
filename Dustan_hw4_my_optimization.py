eb4a9b3b4cf70b2e1e0e7f76bfa8c35c5fe035a2
kraudust <dustan.kraus@gmail.com>
2016-03-01 22:50:07 -0700

    worked on stuff

--- Dustan_hw4_my_optimization.py
+++ Dustan_hw4_my_optimization.py
@@ -3,7 +3,9 @@ from Main_Obj import *
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
-def calc_constraints(xin, params, xupper, yupper):
+def calc_constraints(xin, params):
+	xupper = 1000
+	yupper = 1000
 	xVAWT = xin[0 : nVAWT]
 	yVAWT = xin[nVAWT: 2*nVAWT]
 	xHAWT = xin[2*nVAWT: 2*nVAWT + nHAWT]
@@ -21,38 +23,129 @@ def calc_constraints(xin, params, xupper, yupper):
 		const = np.append(const, yupper - yHAWT)
 	return const
 	
-def uncon_obj(xin, params, xupper, yupper):
-	const = calc_constraints(xin, params, xupper, yupper)
-	sum_log = 0
-	for i in range(0, const):
-		if const[i] == 0:
-			ind_log = 0
-		if const[i] <= 0:
-			ind_log = -1000.
-		ind_log = np.log10(const[i])
-		sum_log = sum_log + ind_log
-	new_obj = obj(xin, params) - mu*sum_log
+def penalty_obj(xin, args):
+	"""
+	Inputs:
+		xin: nparray of form -  (xVAWT, yVAWT, xHAWT, yHAWT)
+		args: nparray containing:
+			nVAWT - number of vertical axis turbines
+			rh - radius of horizontal axis turbines
+			rv - radius of vertical axis turbines
+			rt - radius of tower
+			U_dir - wind direction in radians
+			U_vel - free stream velocity magnitude
+			mu - scaling parameter to penalize constraint violations
+			obj_han - objective function handle
+			con_han - constraint function handle
+	Returns:
+		new_obj: previous objective function plus a log penalty on constraints
+	"""
+	params = args[0:6]
+	mu = args[6]
+	obj_han = args[7]
+	con_han = args[8]
+	const = con_han(xin, params)
+	sum_quad = 0
+	for i in range(0, len(const)):
+	#	if const[i] <= 0:
+	#		const[i] = -1000 #really penalize bad constraints
+		ind_quad = np.min([0, const[i]])**2
+		sum_quad = sum_quad + ind_quad
+	new_obj = obj_han(xin, params) + (mu/2)*sum_quad
 	return new_obj
+
+def run_optimizer(xin, params, obj_han, con_han):
+	"""
+	Inputs:
+		xin: nparray of form -  (xVAWT, yVAWT, xHAWT, yHAWT)
+		params: nparray containing:
+			nVAWT - number of vertical axis turbines
+			rh - radius of horizontal axis turbines
+			rv - radius of vertical axis turbines
+			rt - radius of tower
+			U_dir - wind direction in radians
+			U_vel - free stream velocity magnitude
+		obj_han - objective function handle
+		con_han - constraint function handle
+	Returns:
+		x_opt: optimum x value (nparray)
+	"""
+	mu = 1. 
+	options = {'disp': True, 'maxiter': 2000}
+	args = np.hstack([params, mu, obj_han, con_han])
+	res = minimize(penalty_obj, xin, args = (args,), method = 'BFGS', jac = False, tol = 1e-9, options = options)
+	x_opt = res.x
+	x_dif = 1
+	while x_dif >= 1.e-6:
+		x_prev = x_opt
+		mu = mu*2
+		options = {'disp': True, 'maxiter': 2000}
+		args = np.hstack([params, mu, obj_han, con_han])
+		res = minimize(penalty_obj, x_prev, args = (args,), method = 'BFGS', jac = False, tol = 1e-9, options = options)
+		x_opt = res.x
+		x_dif = obj_han(x_opt, params) - obj_han(x_prev, params)
+	return x_opt
+
+def plot_func(xin, x_opt):
+	xVAWT_opt = x_opt[0 : nVAWT]
+	yVAWT_opt = x_opt[nVAWT: 2*nVAWT]
+	xHAWT_opt = x_opt[2*nVAWT: 2*nVAWT + nHAWT]
+	yHAWT_opt = x_opt[2*nVAWT + nHAWT : len(xin)]
 	
+	xVAWT = xin[0 : nVAWT]
+	yVAWT = xin[nVAWT: 2*nVAWT]
+	xHAWT = xin[2*nVAWT: 2*nVAWT + nHAWT]
+	yHAWT = xin[2*nVAWT + nHAWT : len(xin)]
+	
+	plt.figure()
+	plt.scatter(xVAWT, yVAWT, c = 'r', s = 60)
+	plt.scatter(xVAWT_opt, yVAWT_opt, c = 'r', s = 100)
+	plt.scatter(xHAWT, yHAWT, c = 'b', s = 60)
+	plt.scatter(xHAWT_opt, yHAWT_opt, c = 'b', s = 100)
+	plt.show()
+	
+def test_func(xin, params):
+	sum = 0
+	for i in range(len(xin)):
+		sum += xin[i]**2
+	return sum
+
+def test_con(xin, params):
+	constraints = np.zeros(len(xin))
+	for i in range(len(xin)):
+		constraints[i] = xin[i] - i
+	return constraints
+
 if __name__=="__main__":
-	xHAWT = np.array([0, 0, 0, 300, 300, 300])
-	yHAWT = np.array([0, 150, 300, 0, 150, 300])
-	xVAWT = np.array([150, 75])
-	yVAWT = np.array([150, 225])
+	#xHAWT = np.array([0, 0, 0, 700, 700, 700])
+	#yHAWT = np.array([0, 350, 700, 0, 350, 700])
+	#xVAWT = np.array([350, 350])
+	#yVAWT = np.array([175, 525])
+	xHAWT = np.array([0, 700])
+	yHAWT = np.array([0, 350])
+	xVAWT = np.array([350])
+	yVAWT = np.array([350])
 	#set input variable xin
 	xin = np.hstack([xVAWT, yVAWT, xHAWT, yHAWT])
-
+	xupper = 1000
+	yupper = 1000
+	x0 = [7,11, 17, 23,76]
 	#set input parameters
 	nVAWT = len(xVAWT)
 	nHAWT = len(xHAWT)
 	rh = 40.
 	rv = 3.
 	rt = 5.
-	direction = 30.
+	direction = -95.
 	dir_rad = (direction+90) * np.pi / 180.
 	U_vel = 8.
 	params = [nVAWT, rh, rv, rt, dir_rad, U_vel]
-	print "Original Power: ", obj(xin, params)
+	#print "Original Power: ", -obj(xin, params)*1.e6
+	x_opt = run_optimizer(x0, params, obj, con)
+	print "Old x", x0
+	print "New x", x_opt
+	#plot_func(xin, x_opt)
+	
 	
 	
 	
--- Dustan_hw_4_builtin_optimize.py
+++ Dustan_hw_4_builtin_optimize.py
@@ -4,10 +4,11 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
 if __name__=="__main__":
-	xHAWT = np.array([0, 0, 0, 300, 300, 300])
-	yHAWT = np.array([0, 150, 300, 0, 150, 300])
-	xVAWT = np.array([150, 75])
-	yVAWT = np.array([150, 225])
+	xHAWT = np.array([0, 0, 0, 700, 700, 700])
+	yHAWT = np.array([0, 350, 700, 0, 350, 700])
+	xVAWT = np.array([350, 350])
+	yVAWT = np.array([175, 525])
+	
 	#set input variable xin
 	xin = np.hstack([xVAWT, yVAWT, xHAWT, yHAWT])
 
@@ -17,24 +18,22 @@ if __name__=="__main__":
 	rh = 40.
 	rv = 3.
 	rt = 5.
-	direction = 30.
+	direction = -95. #wind coming from 
 	dir_rad = (direction+90) * np.pi / 180.
 	U_vel = 8.
 	params = [nVAWT, rh, rv, rt, dir_rad, U_vel]
-	print "Original Power: ", obj(xin, params)
+	print "Original Power: ", -obj(xin, params)*1.e6
 	
 	#-------------------------Optimization----------------------------------
 	#set bound constraints
-	xupper = 2000 #upper bound on x direction field coordinate
-	yupper = 1950 #upper bound on y direction field coordinate
+	xupper = 1000 #upper bound on x direction field coordinate
+	yupper = 1000 #upper bound on y direction field coordinate
 	bound_constraints = bounds(xin, params, xupper, yupper)
 	ineq_constraints = {'type': 'ineq', 'fun': con, 'args': (params,)}
 	options = {'disp': True, 'maxiter': 2000}
-	res = minimize(obj, xin, args = (params,), method = 'SLSQP', jac = False, bounds = bound_constraints, constraints = ineq_constraints, tol = 1e-6, options = options)
+	res = minimize(obj, xin, args = (params,), method = 'SLSQP', jac = False, bounds = bound_constraints, constraints = ineq_constraints, tol = 1e-9, options = options)
 	x_opt = res.x
 	con_opt = con(x_opt, params)
-	print x_opt
-	print con_opt
 	
 	#--------------------------Make Plots-----------------------------------
 	xVAWT_opt = x_opt[0 : nVAWT]
@@ -43,10 +42,9 @@ if __name__=="__main__":
 	yHAWT_opt = x_opt[2*nVAWT + nHAWT : len(xin)]
 	
 	plt.figure()
-	plt.scatter(xVAWT, yVAWT, c = 'k')
-	plt.scatter(xVAWT_opt, yVAWT_opt, c = 'r')
-	plt.scatter(xHAWT, yHAWT, c = 'c')
-	plt.scatter(xHAWT_opt, yHAWT_opt, c = 'g')
+	plt.scatter(xVAWT, yVAWT, c = 'r', s = 60)
+	plt.scatter(xVAWT_opt, yVAWT_opt, c = 'r', s = 100)
+	plt.scatter(xHAWT, yHAWT, c = 'b', s = 60)
+	plt.scatter(xHAWT_opt, yHAWT_opt, c = 'b', s = 100)
 	plt.show()
-	
-    
+	print "New Power: ", -obj(x_opt,params)*1.e6
